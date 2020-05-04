37441eb538fd3063f22682021f642e0cce040a9b
milkyboat <1214208054@qq.com>
2020-05-04 13:17:58 +0800

    Update plague-map.js

--- frontend/web/js/plague-map.js
+++ frontend/web/js/plague-map.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    var mymap = L.map('plague-map').setView([35, 0], 2);
+    var mymap = L.map('plague-map').setView([25, 150], 2);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11',
+        id: 'mapbox/light-v9',
         tileSize: 512,
         zoomOffset: -1
     }).addTo(mymap);
