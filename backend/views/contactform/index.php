46ec53617d567bec163c3a382beb3720624b2364
tenkinoko <56966291+tenkinoko@users.noreply.github.com>
2020-05-26 22:06:17 +0800

    Revert "Revert "Contact form accomplished by 袁嘉蔚""
    This reverts commit 96ad4ce5ebefb5cdef24ad0f0a70e99422eed4ad.

---
+++ backend/views/contactform/index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Team:布里啾啾迪布里多,NKU
+ * coding by 袁嘉蔚 1810546,20200509
+ * a view for contactform
+ */
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\ContactformSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Contactforms';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="contactform-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Contactform', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'firstname',
+            'lastname',
+            'wechatid',
+            'phone',
+            'message',
+            //'sex',
+            //'id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
