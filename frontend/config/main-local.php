13401d6b23db0d4db2341f1015a057ea3328a0a3
milkyboat <1214208054@qq.com>
2020-06-02 15:10:21 +0800

    优化移动端显示

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'zGwfEbdD7sRWiKUhFgXEzu6jeJc1yI_O',
+            'cookieValidationKey' => 'd_iA9NVVmSfYnNScvRJzaWztyXQ1ViAl',
         ],
     ],
 ];
