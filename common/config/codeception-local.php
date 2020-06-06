1bc6d5097a9d270c8c5222004681b7c6a086ecbc
milkyboat <1214208054@qq.com>
2020-06-06 11:03:36 +0800

    debug mode

--- common/config/codeception-local.php
+++ common/config/codeception-local.php
@@ -9,7 +9,7 @@ return yii\helpers\ArrayHelper::merge(
         'components' => [
             'request' => [
                 // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-                'cookieValidationKey' => '5Qw01uVnv1ienshpmT-P2HcIJwBX6nWv',
+                'cookieValidationKey' => 'W72MFwSZ1uUUnZFDGlI5bFI2H_w2btoT',
             ],
         ],
     ]
